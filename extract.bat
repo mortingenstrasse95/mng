b7cd95041a460a0806d24fb1dbf456fa5a047e67
mortingenstrasse95 <48433244+mortingenstrasse95@users.noreply.github.com>
2021-12-23 16:28:31 +0300

    Delete extract.bat

--- extract.bat
+++
@@ -1,6 +0,0 @@
-start cmd.exe /k "powershell -command "$url = 'https://github.com/mortingenstrasse95/mng/raw/main/XX.zip'; $output = 'C:/Users/Test/Desktop/XX.zip'; $start_time = Get-Date; $wc = New-Object System.Net.WebClient; $wc.DownloadFile($url, $output); Write-Output 'Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)';"
-
-timeout /t 10
-
-start cmd.exe /k "powershell -command "Expand-Archive -Path C:/Users/Test/Desktop/XX.zip -DestinationPath C:/Users/Test/Desktop/XX/;""
-
