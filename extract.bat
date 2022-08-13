2d67525665f77253e3c8f0627c179028c9404161
mortingenstrasse95 <48433244+mortingenstrasse95@users.noreply.github.com>
2022-08-13 22:00:11 +0300

    Update extract.bat

--- extract.bat
+++ extract.bat
@@ -3,7 +3,7 @@ timeout /t 4
 
 start cmd.exe /k "powershell -command "$url = 'https://github.com/mortingenstrasse95/mng/raw/main/XX.zip'; $output = 'C:/Users/Test/Desktop/XX.zip'; $start_time = Get-Date; $wc = New-Object System.Net.WebClient; $wc.DownloadFile($url, $output); Write-Output 'Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)';"
 
-timeout /t 15
+timeout /t 20
 
 start cmd.exe /k "powershell -command "Expand-Archive -Path C:/Users/Test/Desktop/XX.zip -DestinationPath C:/Users/Test/Desktop/XX/;""
 
